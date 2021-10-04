03c0b4a4fbc594aceed625147b4040c2a935bf4a
Marco Marinò <51913759+marcomarinodev@users.noreply.github.com>
2021-10-04 20:04:42 +0200

    Starting with undergraduate job

--- Ax3/Marco/src/mypackage/App.java
+++ Ax3/Marco/src/mypackage/App.java
@@ -15,13 +15,32 @@ class User {
         System.out.println("User#" + id);
     }
 
+    public void work(String startMessage, String endMessage, int ms) {
+        // print start message
+        System.out.println(startMessage);
+
+        // doing work...
+        ConcurrentUtils.sleep(ms);
+
+        // print exit message
+        System.out.println(endMessage);
+    }
+
+}
+
+class UndergraduateRunnable extends User implements Runnable {
+    
+    public UndergraduateRunnable(int id) { super(id); }
+    
+    @Override
+    public void run() {
+        
+    }
 }
 
 class ProfRunnable extends User implements Runnable {
 
-    public ProfRunnable(int id) {
-        super(id);
-    }
+    public ProfRunnable(int id) { super(id); }
 
     @Override
     public void run() {
--- Ax3/Marco/src/mypackage/Laboratory.java
+++ Ax3/Marco/src/mypackage/Laboratory.java
@@ -5,7 +5,7 @@ import java.util.concurrent.locks.*;
 public class Laboratory {
 
     final int size;
-    int availableRooms;
+    Boolean availableRooms;
     final Lock roomLock;
     public Computer[] computers;
     final Condition isEmpty;
@@ -13,32 +13,48 @@ public class Laboratory {
 
     public Laboratory(int size) {
         this.size = size;
-        availableRooms = 0;
+        availableRooms = true;
         roomLock = new ReentrantLock();
         computers = new Computer[size];
         isEmpty = roomLock.newCondition();
     }
 
     /**
-     * If this function is called, a user tries to get a certain pc
+     * If this function is called, undergraduate tries to get a certain pc
      * in the room.
      * @param index: computer index
      */
-    public int get(int index) throws InterruptedException {
+    public void undergraduateGet(int index, UndergraduateRunnable ur) throws InterruptedException {
         computers[index].computerLock.lock();
 
         try {
+            // Waiting until the desired workstation is not occupied
             while (Boolean.TRUE.equals(computers[index].occupied))
                 computers[index].notOccupied.await();
             
+            // set this workstation as occupied
             computers[index].occupied = true;
+
             
-            return index;
+
+            // set this workstation as not occupied
+            computers[index].occupied = false;
+
+            // signaling the next user that needs this workstation
+            computers[index].notOccupied.signal();
+            
+            return;
         
         } finally {
+
+            // then unlock the workstation
             computers[index].computerLock.unlock();
         }
 
+    }
+
+    public void profGet(String startMessage, String endMessage, int ms) {
+        return;
     }
     
 }
